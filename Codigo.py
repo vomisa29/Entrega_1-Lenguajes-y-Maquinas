89a4d4ee002cf60445005a776a77be954e39cbff
Juancabel <jc.beltrang1@uniandes.edu.co>
2023-02-06 09:56:11 -0500

    Adelanto 1

--- Codigo.py
+++ Codigo.py
@@ -1,8 +1,19 @@
-
+expected_char = {}
+defined_words = {}
 file_name= input("Ingrese el nombre del archivo: ")
 file = open("ExampleCodeLYM.txt", encoding="utf8")
 all_lines = file.readlines()
+long_str = ""
 for line in all_lines:
-    line_string = line.strip()
-    
+    long_str = long_str + line.strip()
+word = ""
+for character in long_str:
+    if character != " ":
+        word = word + character 
+        if word in defined_words:
+            print(word)
+            word == ""
+
+
+
 
