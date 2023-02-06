d872e3e88ed4b05783e8dcb27c6cfd3a5eb9d73b
Juancabel <jc.beltrang1@uniandes.edu.co>
2023-02-06 09:06:43 -0500

    Carga Arreglada xd

--- Codigo.py
+++ Codigo.py
@@ -1,10 +1,8 @@
-import csv
 
 file_name= input("Ingrese el nombre del archivo: ")
-
-csvfile = open(file_name, newline='', encoding="utf8")
-reader = csv.DictReader(csvfile)
+file = open("ExampleCodeLYM.txt", encoding="utf8")
+all_lines = file.readlines()
+for line in all_lines:
+    line_string = line.strip()
     
-for row in reader:
-    for element in row:
-        print("ass")
+
