6c00e1d0e40a0e02d484d37cfab82c7460db103c
Pablo_M <p.mendezm@uniandes.edu.co>
2023-02-06 08:51:02 -0500

    Preliminar

--- Codigo.py
+++ Codigo.py
@@ -1,1 +1,10 @@
-print("ahi va")
+import csv
+
+file_name= input("Ingrese el nombre del archivo: ")
+
+csvfile = open(file_name, newline='', encoding="utf8")
+reader = csv.DictReader(csvfile)
+    
+for row in reader:
+    for element in row:
+        print("ass")
