83e8d0c939e30e52747aa6515369028e2c4c25c2
Juancabel <jc.beltrang1@uniandes.edu.co>
2023-02-10 22:01:34 -0500

    semifuncional jejejeje

--- prueba.py
+++ prueba.py
@@ -23,6 +23,11 @@ def program_start(defined_words:dict, defined_basics:dict, defined_funcs:dict,to
     long_str = ""
     for line in all_lines:
         long_str = long_str + line.strip()
+    string_list = long_str.split(" ")
+    long_str = ""
+    for line in string_list:
+        long_str = long_str + line
+    print(long_str)
     normal_reader(defined_words,defined_basics,defined_funcs,token_lst,long_str)
 
 def dict_check(defined_words:dict, defined_basics:dict, defined_funcs:dict,token_lst:list,long_str:str,word:str):
@@ -33,66 +38,76 @@ def dict_check(defined_words:dict, defined_basics:dict, defined_funcs:dict,token
 def variables(defined_words:dict, defined_basics:dict, defined_funcs:dict,token_lst:list,long_str:str):
     word = ""
     for character in long_str:
-        long_str = long_str[1:]
-        word = word + character 
-        if character == ";":
-            var_list = word.split(",")
-            token_lst.extend(var_list)
-            normal_reader(defined_words, defined_basics, defined_funcs,token_lst,long_str)
-            break
+        if character != " ":
+            long_str = long_str[1:]
+            word = word + character 
+            if character == ";":
+                word = word[:-1]
+                var_list = word.split(",")
+                token_lst.extend(var_list)
+                normal_reader(defined_words, defined_basics, defined_funcs,token_lst,long_str)
 
 def variables_proc(defined_words:dict, defined_basics:dict, defined_funcs:dict,token_lst:list,long_str:str):
     word = ""
     for character in long_str:
-        long_str = long_str[1:]
-        word = word + character 
-        if character == "|":
-            var_list = word.split(",")
-            token_lst.extend(var_list)
-            break
+        if character != " ":
+            long_str = long_str[1:]
+            word = word + character 
+            if character == "|":
+                word = word[:-1]
+                var_list = word.split(",")
+                token_lst.extend(var_list)
     
 def procedures(defined_words:dict, defined_basics:dict, defined_funcs:dict,token_lst:list,long_str:str):
     word = ""
     for character in long_str:
-        long_str = long_str[1:]
-        word = word + character 
-        if character == "[":
-            token_lst.append("PROCID("+word+")")
-            inside_proced(defined_words, defined_basics, defined_funcs,token_lst,long_str)
-            break
+        if character != " ":
+            long_str = long_str[1:]
+            word = word + character 
+            if character == "[":
+                word = word[:-1]
+                token_lst.append("PROCID("+word+")")
+                inside_proced(defined_words, defined_basics, defined_funcs,token_lst,long_str)
+                break 
 
 def inside_proced(defined_words:dict, defined_basics:dict, defined_funcs:dict,token_lst:list,long_str:str):
     word = ""
     scuareCounter =0
     for character in long_str:
-        word = word + character
-        if character == "|" and len(word) > 1:
-            raise Exception("Hay un procedimiento mal definido pero no se cual jijijiji")
-        elif character == "|" and len(word) == 1:
-            variables_proc(defined_words, defined_basics, defined_funcs,token_lst,long_str)
-        if character == "[":
-            scuareCounter += 1
-        if character == "]":
-            scuareCounter -= 1 
-            if scuareCounter == 0:
-                procedures(defined_words, defined_basics, defined_funcs,token_lst,long_str)
-                break
+        if character != " ":
+            long_str = long_str[1:]
+            word = word + character
+            if character == "|":
+                word = word[:-1]
+                variables_proc(defined_words, defined_basics, defined_funcs,token_lst,long_str)
+            if character == "[":
+                word = word[:-1]
+                scuareCounter += 1
+            if character == "]":
+                word = word[:-1]
+                scuareCounter -= 1 
+                if scuareCounter == 0:
+                    print(long_str)
+                    procedures(defined_words, defined_basics, defined_funcs,token_lst,long_str)
+                    break
         
 def rob(defined_words:dict, defined_basics:dict, defined_funcs:dict,token_lst:list,long_str:str):
-    word = ""
     if len(token_lst) >0:
         if token_lst[0] != "ROB":
             raise Exception("Este no es un codigo ROBOT_R")
+    normal_reader(defined_words,defined_basics,defined_funcs,token_lst,long_str)
 
 def normal_reader(defined_words:dict, defined_basics:dict, defined_funcs:dict,token_lst:list,long_str:str): 
     word = ""
     for character in long_str:
-        long_str = long_str[1:] 
-        word = word + character
-        if word in defined_words:
-            token_lst.append(defined_words[word])
-            defined_funcs[word](defined_words, defined_basics, defined_funcs,token_lst,long_str)
-            break 
+        if character != " ":
+            long_str = long_str[1:]
+            word = word + character
+            if word in defined_words:
+                token_lst.append(defined_words[word])
+                defined_funcs[word](defined_words, defined_basics, defined_funcs,token_lst,long_str)
+                word = ""
+                break 
 
 #Definicion de diccionario de funciones 
 
@@ -104,6 +119,12 @@ defined_funcs = {"M:":"M", "R:":"R","C:":"C","B:":"B","c:":"c","b:":"b","P:":"P"
 #Start 
 program_start(defined_words, defined_basics, defined_funcs,token_lst)
 
+def recursive_experiment(defined_words, defined_basics, defined_funcs,token_lst):
+    file = open("ExampleCodeLYM.txt", encoding="utf8")
+    all_lines = file.readlines()
+    long_str = ""
+    for line in all_lines:
+        long_str = long_str + line.strip()
+    return token_lst
 
-
-
+print(token_lst)
