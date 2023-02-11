9961430fda1f2619d522e70c1ffaeadae9776e18
Juancabel <jc.beltrang1@uniandes.edu.co>
2023-02-10 20:51:09 -0500

    non func update

--- prueba.py
+++ prueba.py
@@ -1,26 +1,37 @@
-expected_char = {}
+"""
+***********************************************************************************************************************************************************
+DEFINICIONES DE DICCIONARIOS
+***********************************************************************************************************************************************************
+"""
+
 defined_words = {"M:":"M", "R:":"R","C:":"C","B:":"B","c:":"c","b:":"b","P:":"P","J(":"funcJ","G(":"funcG",
 "PROCS":"PROCS","ROBOT_R":"ROB","VARS":"VARS"}
 defined_basics = {"assignTo:":"procAST","goto:":"procGOT", "move:":"procMOV", 
 "turn:":"procTUR", "face:":"procFAC", "put:":"procPUT", "pick:":"procPIC", "moveToThe:":"procMTT","moveInDir:":"procMID",
 "jumpToThe:":"procJTT","jumpInDir:":"procJID", "nop":"procNOP", "while":"while", "repeat":"repeat"}
-global long_str
-global token_lst
-global defined_funcs
 token_lst = []
-file_name= input("Ingrese el nombre del archivo: ")
-file = open("ExampleCodeLYM.txt", encoding="utf8")
-all_lines = file.readlines()
-long_str = ""
-for line in all_lines:
-    long_str = long_str + line.strip()
+
+"""
+***********************************************************************************************************************************************************
+DEFINICIONES DE FUNCIONES
+***********************************************************************************************************************************************************
+"""
+
+def program_start(defined_words:dict, defined_basics:dict, defined_funcs:dict):
+    file = open("ExampleCodeLYM.txt", encoding="utf8")
+    all_lines = file.readlines()
+    global long_str
+    long_str = ""
+    for line in all_lines:
+        long_str = long_str + line.strip()
+    normal_reader(defined_words,defined_basics,defined_funcs,token_lst)
 
 def dict_check(word:str):
     if word in defined_words:
             token_lst.append(defined_words[word])
             defined_funcs[word]
 
-def variables(long_str):
+def variables():
     word = ""
     for character in long_str:
         long_str = long_str[1:]
@@ -28,10 +39,10 @@ def variables(long_str):
         if character == ";":
             var_list = word.split(",")
             token_lst.extend(var_list)
-            normal_reader(long_str)
+            normal_reader()
             break
 
-def variables_proc(long_str):
+def variables_proc():
     word = ""
     for character in long_str:
         long_str = long_str[1:]
@@ -41,8 +52,7 @@ def variables_proc(long_str):
             token_lst.extend(var_list)
             break
     
-
-def procedures(long_str):
+def procedures():
     word = ""
     for character in long_str:
         long_str = long_str[1:]
@@ -52,9 +62,7 @@ def procedures(long_str):
             inside_proced(long_str)
             break
 
-
-
-def inside_proced(long_str):
+def inside_proced(defined_words:dict, defined_basics:dict, defined_funcs:dict,token_lst:list):
     word = ""
     scuareCounter =0
     for character in long_str:
@@ -62,37 +70,41 @@ def inside_proced(long_str):
         if character == "|" and len(word) > 1:
             raise Exception("Hay un procedimiento mal definido pero no se cual jijijiji")
         elif character == "|" and len(word) == 1:
-            variables_proc(long_str)
+            variables_proc(defined_words:dict, defined_basics:dict, defined_funcs:dict,token_lst:list)
         if character == "[":
             scuareCounter += 1
         if character == "]":
             scuareCounter -= 1 
             if scuareCounter == 0:
-                procedures(long_str)
+                procedures(defined_words:dict, defined_basics:dict, defined_funcs:dict,token_lst:list)
                 break
         
-def rob():
+def rob(defined_words:dict, defined_basics:dict, defined_funcs:dict,token_lst:list):
     word = ""
     if len(token_lst) >0:
         if token_lst[0] != "ROB":
             raise Exception("Este no es un codigo ROBOT_R")
 
-def normal_reader(long_str): 
+def normal_reader(defined_words:dict, defined_basics:dict, defined_funcs:dict,token_lst:list): 
     word = ""
     for character in long_str:
         long_str = long_str[1:] 
         word = word + character
         if word in defined_words:
             token_lst.append(defined_words[word])
-            defined_funcs[word](";")
+            defined_funcs[word](defined_words:dict, defined_basics:dict, defined_funcs:dict,token_lst:list)
             break 
+
+#Definicion de diccionario de funciones 
+
 defined_funcs = {"M:":"M", "R:":"R","C:":"C","B:":"B","c:":"c","b:":"b","P:":"P","J(":"funcJ","G(":"funcG",
-"PROCS":"PROCS","ROBOT_R":rob(),"VARS":variables(long_str),"assignTo:":"procAST","goto:":"procGOT", "move:":"procMOV", 
+"PROCS":"PROCS","ROBOT_R":rob,"VARS":variables,"assignTo:":"procAST","goto:":"procGOT", "move:":"procMOV", 
 "turn:":"procTUR", "face:":"procFAC", "put:":"procPUT", "pick:":"procPIC", "moveToThe:":"procMTT","moveInDir:":"procMID",
 "jumpToThe:":"procJTT","jumpInDir:":"procJID", "nop":"procNOP", "while":"while", "repeat":"repeat"}
 
-normal_reader(long_str)
-
+#Start 
+params = [defined_basics,defined_funcs,defined_basics, token_lst]
+program_start(params)
 
 
 
